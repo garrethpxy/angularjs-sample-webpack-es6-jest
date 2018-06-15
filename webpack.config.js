cf7e28a791a4d9aac6cd5710f78341a534807e89
Garreth Peh <garrethpeh@gmail.com>
2018-06-15 20:52:18 +0800

    add scss stylesheets

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,38 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/index.js"],
-  output: {
-    path: path.join(__dirname, "build/"),
-    filename: "bundle.js"
-  },
-  module: {
-    strictExportPresence: true,
-    rules: [
-      // Load raw HTML files for templates
-      { test: /\.(html)$/, loader: "raw-loader" },
-      // Load js files through Babel
-      { test: /\.(js|jsx)$/,   loader: "babel-loader" }
+    entry: ["./src/index.js"],
+    output: {
+        path: path.join(__dirname, "build/"),
+        filename: "bundle.js"
+    },
+    module: {
+        strictExportPresence: true,
+        rules: [
+            // Load raw HTML files for templates
+            { test: /\.(html)$/, loader: "raw-loader" },
+            // Load js files through Babel
+            { test: /\.(js|jsx)$/,   loader: "babel-loader" },
+            // Load SCSS
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader'],
+                    publicPath: '../'
+                })
+            }
+        ]
+    },
+    plugins: [
+        // Generates an `index.html` file with the <script> injected.
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "src/index.html",
+            filename: "index.html"
+        }),
+        new ExtractTextPlugin('css/style.css')
     ]
-  },
-  plugins: [
-    // Generates an `index.html` file with the <script> injected.
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: "src/index.html",
-      filename: "index.html"
-    }),
-  ]
 }
